34d64378c8a275e656659b3d3540513f8cf9deee
Tsim Muaj Yang <tyang4204@gmail.com>
2026-02-05 06:18:34 -0600

    added authentication

--- app/ui/topics/new/page.tsx
+++ app/ui/topics/new/page.tsx
@@ -1,3 +1,10 @@
-export default function Page(){
-    return <div>New Topics Page</div>
+import CreateTopicForm from "@/components/CreateTopicForm";
+
+export default function Page() {
+  return (
+    <div>
+      <h1 className={`mb-4 text-xl md:text-2xl`}>New Topic</h1>
+      <CreateTopicForm />
+    </div>
+  );
 }
