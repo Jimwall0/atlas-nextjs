e11622ced9bf169c5e0c478d3139d45240aaa8d6
Tsimmuaj <tyang4204@gmail.com>
2026-02-05 09:29:01 -0600

    fixed import issues

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,7 +2,7 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { insertTopic } from "./data";
+import { insertTopic, insertQuestion, incrementVotes } from "./data";
 import { redirect } from "next/navigation";
 
 export async function addTopic(data: FormData) {
